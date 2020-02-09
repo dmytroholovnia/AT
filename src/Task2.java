fdcb61888a89d2866e17645d81a4bcd77caec54a
dmytroholovnia <dmytro.holovnia.com>
2020-02-09 22:57:44 +0200

    Task2

---
+++ src/Task2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner
+
+public class module2_exchange {
+    public static void main (String[] args) {
+
+        double income;
+
+        System.out.println("You invest in our project and we'll double your money!");
+        Scanner in = new Scanner(System.in);
+        double investment = in.nextDouble();
+
+        System.out.println("Some more money..");
+        double moreInvestment = in.nextDouble();
+
+        income = investment;
+        investment = moreInvestment;
+        moreInvestment = income;
+
+        System.out.println(income);
+
+
+
+
+
+    }
+}
