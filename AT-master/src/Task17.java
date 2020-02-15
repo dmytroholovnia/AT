3d0862f64253f9e6b14a8f13e52ca02772eb8a10
dmytroholovnia <dmytro.holovnia.com>
2020-02-15 22:20:36 +0200

    Task18

--- AT-master/src/Task17.java
+++ AT-master/src/Task17.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Task17 {
-}
+    public static void main (String [] args) {
+        int pred = 5;
+        int number = 10;
+        int sum = 0;
+
+        for (int i = 0; i <= pred; i++) {
+            for (int j = 0; j <= pred; j++) {
+                for (int k = 0; k <= pred; k++) {
+                    sum = i + j + k;
+                    if (sum == number) {
+                        System.out.println(i + " " + j + " " + k);
+                    }
+                }
+
+            }
+        }
+    }
+
+    }
---
+++ AT-master/src/Task18.java
@@ -0,0 +1,9 @@
+public class Task18 {
+    public static void main (String [] args) {
+        for (int j = 32; j < 127; j++) {
+            System.out.printf("%4d ", j);
+            System.out.println((char) j);
+        }
+    }
+
+}
